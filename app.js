f15d9f874874de26d1d29bd29b040986fc41de43
ppla.fs <sukrit.nak@gmail.com>
2021-08-14 20:55:23 +0700

    [Mod] redis with callback

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require('express')
 const axios = require('axios')
-const app = express()
 const redis = require('redis')
-// app.get((req, res) => {
-//     res.json({
-//         message: 'OK'
-//     })
-// })
+const app = express()
 
-app.get('/', async (req, res) => {
+const redisClient = redis.createClient();
+// default port 6379
+
+app.get('/', (req, res) => {
     const username = req.query.username || 'sukritnak'
     const url = `https://api.github.com/users/${username}`
-    const response = await axios.get(url)
-    res.json(response.data)
+
+    redisClient.get(username, async (err, reply) => {
+        if (reply) {
+            console.log('Have cache')
+            return res.json(JSON.parse(reply))
+        }
+        const response = await axios.get(url)
+        redisClient.setex(username, 60, JSON.stringify(response.data))
+        return res.json(response.data)
+    })
+
 })
 
 app.listen(9000, () => {
