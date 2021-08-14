e49bcb69f2553ac80fc4fa4a4633b125e250729e
ppla.fs <sukrit.nak@gmail.com>
2021-08-14 21:00:40 +0700

    [Mod] modify redisClient to promise

--- app.js
+++ app.js
@@ -2,23 +2,38 @@ const express = require('express')
 const axios = require('axios')
 const redis = require('redis')
 const app = express()
+const { promisify } = require('util');
 
 const redisClient = redis.createClient();
 // default port 6379
 
-app.get('/', (req, res) => {
+const asyncRedisGet = promisify(redisClient.get).bind(redisClient)
+
+app.get('/', async (req, res) => {
+    // app.get('/', (req, res) => {
     const username = req.query.username || 'sukritnak'
     const url = `https://api.github.com/users/${username}`
+    const reply = await asyncRedisGet(username);
 
-    redisClient.get(username, async (err, reply) => {
-        if (reply) {
-            console.log('Have cache')
-            return res.json(JSON.parse(reply))
-        }
-        const response = await axios.get(url)
-        redisClient.setex(username, 60, JSON.stringify(response.data))
-        return res.json(response.data)
-    })
+    if (reply) {
+        console.log('Have cache')
+        return res.json(JSON.parse(reply))
+    }
+
+    const response = await axios.get(url)
+    redisClient.setex(username, 60, JSON.stringify(response.data))
+    return res.json(response.data)
+
+
+    // redisClient.get(username, async (err, reply) => {
+    //     if (reply) {
+    //         console.log('Have cache')
+    //         return res.json(JSON.parse(reply))
+    //     }
+    //     const response = await axios.get(url)
+    //     redisClient.setex(username, 60, JSON.stringify(response.data))
+    //     return res.json(response.data)
+    // })
 
 })
 
